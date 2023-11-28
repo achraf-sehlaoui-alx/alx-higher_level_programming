3a8d2bd129df0b0cce4fee610102b9ca6cd26d4f
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-11-28 12:25:13 +0000

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     if ord(c) >= ord('a') and ord(c) <= ord('z'):
-        return true
+        return True
     else:
-        return false
+        return False
