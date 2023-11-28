2fd1f6aa9d7c3b67f8cad77345c9f6167f6f42c1
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-11-28 19:20:48 +0000

    test

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a'), -2):
-    print("{}{}".format(i, str.upper(i)), end="")
+for i in range(ord('z'), ord('a'), -1):
+    if i % 2:
+        i = i - 32
+    print("{:c}".format(i), end="")
