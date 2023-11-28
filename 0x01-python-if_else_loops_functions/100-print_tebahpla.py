f0d4533e8514d1be8d7fcc39a63f8c8a3e40169e
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-11-28 19:24:18 +0000

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2:
-        i = i - 32
+for i in range(25, -1, -1):
+    c = i + ord('A')
+    if i % 2 == 1:
+        i = i + 32
     print("{:c}".format(i), end="")
