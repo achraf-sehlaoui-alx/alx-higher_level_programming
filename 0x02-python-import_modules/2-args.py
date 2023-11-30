73cae594e527b60e57faa3c8c38a73c9c5b7bdb9
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-11-30 14:16:46 +0000

    script dynamic?

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     z = len(sys.argv)
     if z == 1:
-        print("{} arguments.".format(z - 1))
+        print("0 arguments.")
     elif z == 2:
-        print("{} argument:".format(z - 1))
+        print("1 argument:")
     else:
         print("{} arguments:".format(z - 1))
     for i in range(1, z):
