57cb372e64a838868462d7577ed9c901f16dbac6
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-12-03 14:55:04 +0000

    Find the max_

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,6 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     else:
-        x = 0
-        for i in my_list:
-            if i > x:
-                x = i
-        return x
+        new_list = my_list.copy()
+        new_list.sort()
+        return new_list[-1]
