da0a5e1ef32cab9e5f749dd6c69e2c9d3aa9634d
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-12-18 22:25:24 +0000

    2/1/2024

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    n = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            n += 1
+        except (TypeError, ValueError):
+            pass
+        i += 1
+    print()
+    return n
