2dd20dec7c779efaeef49b943b202d8cd61eebfb
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-12-18 22:10:52 +0000

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         while i is not x:
-            print(my_list=[i], end='')
-            i++
+            print(my_list[i], end='')
+            i += 1
     except IndexError:
         None
     print()
