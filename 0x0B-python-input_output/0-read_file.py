6b1903e5102b933b5c79ad2d508c79cd6599d5ec
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2024-01-09 19:48:45 +0000

    11

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""defining read_file function"""
+
+
 def read_file(filename=""):
-    """ function that reads a text file (UTF8) and prints it to stdout """
+    """ function that reads a text file UTF-8"""
 
     with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
