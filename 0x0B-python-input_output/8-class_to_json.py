7c55b5fc920d7d5edfee9b477ba01ee334b1bcc7
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2024-01-09 20:10:17 +0000

    999

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""8-class_to_json.py"""
+"""8-class_to_json.py 8-class_to_json.py"""
 
 
 def class_to_json(obj):
-    """8-class_to_json.py"""
-    return vars(obj)
+    """8-class_to_json.py 8-class_to_json.py
+    8-class_to_json.py 8-class_to_json.py"""
+    return obj.__dict__
