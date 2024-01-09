1604ba26dad4817580914a40af1aa4c21a0e24d9
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2024-01-09 19:55:57 +0000

    2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""defining append_write function"""
 
 
 def append_write(filename="", text=""):
+    """append_write utf-8"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""to_json_string"""
 
 
 import json
 
 
 def to_json_string(my_obj):
+    """to_json_string of an object"""
     return json.dumps(my_obj)
