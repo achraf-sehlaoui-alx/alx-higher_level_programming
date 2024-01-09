c489a408a55dbc5f52de308bf41ed6e3feea4fb1
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2024-01-09 20:46:13 +0000

    f

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,15 +3,17 @@
 
 
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arglist = list(sys.argv[1:])
 
 try:
-    old_data = load_from_json_file('add_item.json')
+    old_data = load_from_json_file("add_item.json")
 except Exception:
     old_data = []
 
 os.path.exists(arglist)
-save_to_json_file(old_data, 'add_item.json')
+save_to_json_file(old_data, "add_item.json")
