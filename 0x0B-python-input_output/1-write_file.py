fb8ffbf3bfcabdb279d167433c35e5284b293d70
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2024-01-09 19:52:18 +0000

    1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""defining write_file with two args"""
 
 
 def write_file(filename="", text=""):
+    """reads filnams with utf-8"""
     with open(filename, "w", encoding="UTF-8") as f:
         return f.write(text)
