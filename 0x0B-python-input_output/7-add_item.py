235b8fc063139b7aabee3368e3fd54e2c0169d72
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2024-01-09 20:05:50 +0000

    5

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,21 +2,17 @@
 """Module for saving to json"""
 
 
-import json
-import os.path
 import sys
-from sys import argv
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-json_list = []
+arglist = list(sys.argv[1:])
 
-if os.path.exists(filename):
-    json_list = load_from_json_file(filename)
+try:
+    old_data = load_from_json_file('add_item.json')
+except Exception:
+    old_data = []
 
-for index in argv[1:]:
-    json_list.append(index)
-
-save_to_json_file(json_list, filename)
+os.path.exists(arglist)
+save_to_json_file(old_data, 'add_item.json')
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""8-class_to_json.py"""
 
 
 def class_to_json(obj):
+    """8-class_to_json.py"""
     return vars(obj)
