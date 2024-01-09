dd14d38ac52ae5eb5f8f6be2aee730b7bb277084
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2024-01-09 20:54:54 +0000

    w

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
+
+
 import sys
 
 if __name__ == "__main__":
@@ -11,5 +13,6 @@ if __name__ == "__main__":
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
+    
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
