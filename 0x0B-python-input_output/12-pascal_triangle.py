c395550f4273e05ed8368edf85cd4fada9338059
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2024-01-09 20:48:38 +0000

    3

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""Pascal's Triangle Pascal's Triangle"""
+"""Defines a Pascal's Triangle function."""
 
 
-class student:
-    """Pascal's Triangle Pascal's Triangle"""
-    def __init__(self, first_name, last_name, age):
-        """Pascal's Triangle Pascal's Triangle"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
 
-    def to_json(self):
-        """last_name last_name"""
-        return self.__dict__
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Module for saving to json"""
+"""Add all arguments to a Python list and save them to a file."""
 
 
 import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-arglist = list(sys.argv[1:])
-
-try:
-    old_data = load_from_json_file("add_item.json")
-except Exception:
-    old_data = []
-
-os.path.exists(arglist)
-save_to_json_file(old_data, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
